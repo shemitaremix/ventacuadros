12b1bce39d7afb97635287dde358e94aee149c6f
Shemitaremix <israel_1320114066@uptecamac.edu.mx>
2021-11-08 19:00:57 -0600

    si señor

--- forms/contact.php
+++ forms/contact.php
@@ -47,7 +47,7 @@ try {
   $mail->isSMTP();                                            //Send using SMTP
   $mail->Host       = 'smtp.outlook.com';                     //Set the SMTP server to send through
   $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-  $mail->Username   = 'israel_1320114066@uptecamac.edu.mx';                     //SMTP username
+  $mail->Username   = '';                     //SMTP username
   $mail->Password   = 'Tambo2002';                               //SMTP password
   $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
   $mail->Port       = 587;                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
